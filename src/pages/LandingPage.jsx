f4cc98341a37f684c0c6039f3ba49f0300752958
Shobhit sharma <shobhitss652@gmail.com>
2025-10-01 09:34:21 +0530

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -34,7 +34,7 @@ const LandingPage = () => {
       <div className='w-full min-h-full bg-[#F7CFD8]'>
         <div className='w-[500px] h-[500px] bg-[#A53860] blur-[300px] absolute top-0 left-0'/>
 
-        <div className='w-9/10 container mx-auto px-4 pt-6 pb-[200px] relative z-10'>
+        <div className='max-w-7xl mx-auto px-4 pt-6 pb-[200px] relative z-10'>
           { /* Headers */ }
           <header className='flex justify-between items-center mb-16'>
             <div className='text-xl text-black font-bold'>
@@ -50,15 +50,16 @@ const LandingPage = () => {
           </header>
 
           {/* Hero Content */}
-          <div className='flex flex-col md:flex-row items-center'>
+       <div className='w-full md:w-1/2 pr-4 mb-8 md:mb-0 text-center md:text-left'>
+
             <div className='w-full md:w-1/2 pr-4 mb-8 md:mb-0'>
               <div className='flex items-center justify-left mb-2'>
                 <div className='flex items-center gap-2 text-[13px] text-[#670D2F] font-semibold bg-[#F7CFD8] px-3 py-1 rounded-full border border-[#7D1C4A]'>
                   <LuSparkles /> AI Powered
                 </div>
               </div>
+              <h1 className='text-3xl md:text-5xl text-black font-medium mb-6 leading-tight'>
 
-              <h1 className='text-5xl text-black font-medium mb-6 leading-tight'>
                 Ace Interviews with <br/>
                 <span className='text-transparent bg-clip-text bg-[radial-gradient(circle,_#7D1C4A_0%,_#670D2F_100%)] bg-[length:200%_200%] animate-text-shine font-semibold'>
                   AI-Powered
@@ -75,7 +76,7 @@ const LandingPage = () => {
                 here.
               </p>
 
-              <button className='bg-black text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-[#FFEEF4] hover:text-black border border-[#F7CFD8] hover:border-[#670D2F] transition-colors cursor-pointer' onClick={handleCTA}>
+              <button className='bg-black text-sm font-semibold text-white px-7 py-2.5 w-full sm:w-auto rounded-full hover:bg-[#FFEEF4] hover:text-black border border-[#F7CFD8] hover:border-[#670D2F] transition-colors cursor-pointer' onClick={handleCTA}>
                 Get Started
               </button>
 
@@ -86,8 +87,8 @@ const LandingPage = () => {
 
       <div className='w-full bg-[#F7CFD8] min-h-full relative z-10'>
         <div>
-          <section className='flex items-center justify-center -mt-36'>
-            <img src={HERO} alt="Hero Image" className='w-[80vw] rounded-lg' />
+       <section className='flex items-center justify-center px-4 -mt-36'>
+            <img src={HERO} alt="Hero Image" className='w-full max-w-5xl rounded-lg' />
           </section>
         </div>
 
@@ -99,7 +100,7 @@ const LandingPage = () => {
               </h2>
               <div className='flex flex-col items-center gap-8'>
                 {/* First 3 Cards */}
-                <div className='grid grid-cols-1 md:grid-cols-3 gap-8 w-full'>
+               <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full'>
                   {APP_FEATURES.slice(0,3).map((feature) => (
                     <div
                       key={feature.id}
@@ -113,7 +114,7 @@ const LandingPage = () => {
 
                 {/* Remaining 2 cards */}
 
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+               <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full'>
                   {APP_FEATURES.slice(3).map((feature) => (
                     <div
                       key={feature.id}
