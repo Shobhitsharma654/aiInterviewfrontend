bbd572b66c0b5e465a48b5b78fe8b4f8eb1f82c8
Shobhit Sharma <shobhitss172@gmail.com>
2025-12-01 22:06:16 +0530

    latest update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    // Increase chunk size limit from 500kb → 1500kb
+    chunkSizeWarningLimit: 1500,
+
+    // Optional but recommended: split node_modules into vendor.js
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"
+          }
+        }
+      }
+    }
+  }
 })
